4f3633d2be5e182f1ea2699fd3e2007945728563
Abdulmalik <ayodejiamzat@gmail.com>
2024-05-28 23:50:28 +0100

    auto-commit

--- models/Order.js
+++ models/Order.js
@@ -16,7 +16,7 @@ const orderSchema = mongoose.Schema(
             type: mongoose.Schema.Types.ObjectId,
 			ref: 'User',
         },
-		orderNotes: { type: String, require: true},
+		orderNote: { type: String, require: true},
 		status: { type: String, require: true, enum: ["Pending", "Approved", "Shipped", "Delivered", "Canceled"], default: "Pending"},
 	},
 	{
