d9471a8b8b2fe8805c47244ce38ea74cc562d071
Abdulmalik <ayodejiamzat@gmail.com>
2024-05-29 23:26:30 +0100

    auto-commit

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -21,7 +21,7 @@ router.route('/updateitem/:id')
     .patch(adminProtect, update_product)
 router.route('/updatereview/:id')
     .patch(adminProtect, update_review)
-router.route('/getorderid')
+router.route('/getorder/:')
     .patch(adminProtect, update_review)
 
 export default router
