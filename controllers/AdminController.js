1ac49317cd45aff4b90528cc77bd0cc5488b6fbf
Abdulmalik <ayodejiamzat@gmail.com>
2024-05-31 01:15:43 +0100

    auto-commit

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -322,7 +322,7 @@ export const update_order = asyncHandler(async (req, res, next) => {
 			res.status(404).json({ message: 'Oreder not found' });
 			return;
 		}
-		const { approved = review.approved } = req.body;
+		const { status = review.approved } = req.body;
 		review.approved = approved;
 		const updated = await review.save()
 		if (updated) {
